6c6abd1ce926797338b092c8d8ee90b37739adb0
imranm3 <imranm3@hickory.cs.vcu.edu>
2023-07-26 17:42:19 -0400

    readme added

--- contrastive_learning/contrastive_learning.py
+++ contrastive_learning/contrastive_learning.py
@@ -1,67 +1,25 @@
 import string
 import pandas as pd
-from transformers import AutoTokenizer, AutoModel
 import torch
 import re
-import csv
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-import torch.nn as nn
-import pandas as pd
-from transformers import AutoTokenizer
-import torch
-from transformers import AutoTokenizer
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoModelForSequenceClassification
-import nltk
-import numpy as np
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-from sklearn.metrics import classification_report
-from nltk.corpus import wordnet
-from nltk import pos_tag
-nltk.download('maxent_treebank_pos_tagger')
-nltk.download('averaged_perceptron_tagger')
-
-import re
-from transformers import get_linear_schedule_with_warmup
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
 import sys
-import string
-import re
+import argparse
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, AutoModel
 
-flag = int(sys.argv[1])
-print(flag)
+def select_pretrained_model(model_name, output_save_path):
+    models = {
+        "bert": ("bert-base-uncased", f"{output_save_path}/bert_model_and_optimizer.pt"),
+        "roberta": ("roberta-base", f"{output_save_path}/roberta_model_and_optimizer.pt"),
+        "albert": ("albert-base-v2", f"{output_save_path}/albert_model_and_optimizer.pt"),
+        "codebert": ("microsoft/codebert-base", f"{output_save_path}/codebert_model_and_optimizer.pt"),
+    }
+    if model_name not in models:
+        raise ValueError("Invalid model value. Use bert, roberta, albert, codebert.")
+    return models[model_name]
 
-if flag == 0:
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    model = AutoModel.from_pretrained("bert-base-uncased")
-    saved_file = 'bert_model_and_optimizer.pt'
-elif flag == 1:
-    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-    model = AutoModel.from_pretrained("roberta-base")
-    saved_file = 'roberta_model_fig_lan.pt'
-elif flag == 2:
-    tokenizer = AutoTokenizer.from_pretrained("albert-base-v2")
-    model = AutoModel.from_pretrained("albert-base-v2")
-    saved_file = 'albert_model_and_optimizer.pt'
-
-model.to(device)
-print(saved_file)
-
-# =======================================================================================================
-# READING DATASET
-
-def get_data():
-    dataframe = pd.read_csv("Fig_Lan_Annotation.csv")
+def get_data(data_file):
+    dataframe = pd.read_csv(data_file)
     print(dataframe.keys())
 
     dataframe = dataframe.dropna()
@@ -71,196 +29,26 @@ def get_data():
     print(dataframe.columns)
     return dataframe
 
+def prepare_dataset(dataframe, tokenizer, max_len=128):
+    texts = []
+    for i in range(len(dataframe['Sentence'].values.tolist())):
+        sentences = dataframe.iloc[i].values.tolist()
+        texts.append([str(sentences[1]), str(sentences[3]), str(sentences[4])])
 
-dataframe = get_data()
+    dataframe = pd.DataFrame(texts, columns=['anchor', 'positive', 'negative'])
 
+    ancors, positives, negatives = [], [], []
+    for row in texts:
+        ancors.append(row[0])
+        positives.append(row[1])
+        negatives.append(row[2])
+        ancors.append(row[1])
+        positives.append(row[0])
+        negatives.append(row[2])
 
-
-
-def text_cleaning(text):
-
-
-    def remove_block_quotes(text):
-        modified_text = ''
-        prev_line_block = False
-        for line in text.split('\n'):
-            if not line.strip().startswith('>'):
-                modified_text += line + '\n'
-                prev_line_block = False
-            else:
-                if prev_line_block is True:
-                    continue
-                else:
-                    modified_text += '[BLOCK QUOTE].' + '\n'
-                    prev_line_block = True
-        return modified_text
-
-
-    def remove_newlines(text):
-        # replace new lines with space
-        modified_text = text.replace("\n", " ")
-        return modified_text
-
-
-    def remove_extra_whitespaces(text):
-        return ' '.join(text.split())
-
-    def remove_stopwords(text):
-        # Tokenize the text into individual words
-        tokens = word_tokenize(text)
-        
-        # Get the list of English stopwords
-        stop_words = set(stopwords.words('english'))
-        
-        # Filter out the stopwords
-        filtered_tokens = [word for word in tokens if word.casefold() not in stop_words]
-        
-        # Join the filtered tokens back into a single string
-        filtered_text = ' '.join(filtered_tokens)
-        
-        return filtered_text
-
-
-
-    def remove_triple_quotes(text):
-        occurrences = [m.start() for m in re.finditer('```', text)]
-        idx = len(occurrences)
-        if idx % 2 == 1:
-            text = text[:occurrences[idx - 1]]
-            idx = idx - 1
-        for i in range(0, idx, 2):
-            if idx > 0:
-                text = text[:occurrences[idx - 2]] + '[TRIPLE QUOTE].' + text[(occurrences[idx - 1] + 3):]
-                idx = idx - 2
-        return text
-
-
-    def remove_stacktrace(text):
-        st_regex = re.compile('at [a-zA-Z0-9\.<>$]+\(.+\)')
-        lines = list()
-        for line in text.split('\n'):
-            matches = st_regex.findall(line.strip())
-            if len(matches) == 0:
-                lines.append(line)
-            else:
-                for match in matches:
-                    line = line.replace(match, ' ')
-                lines.append(line.strip(' \t'))
-
-        lines = '\n'.join(lines)
-        # hack to get rid of multiple spaces in the text
-        # lines = ' '.join(lines.split())
-        return lines
-
-
-    def remove_url(text):
-        text = re.sub(r"http\S+", "[URL]", text)
-        return text
-
-
-    def remove_usermention(text):
-        text = re.sub(' @[^\s]+', ' [USER]', text)
-        if text.startswith('@'):
-            text = re.sub('@[^\s]+', '[USER]', text)
-        return text
-
-    def lemmatize_text(text):
-        # Tokenize the text into individual words
-        tokens = word_tokenize(text)
-        
-        # Map POS tags to WordNet tags
-        def get_wordnet_pos(tag):
-            if tag.startswith('J'):
-                return wordnet.ADJ
-            elif tag.startswith('V'):
-                return wordnet.VERB
-            elif tag.startswith('N'):
-                return wordnet.NOUN
-            elif tag.startswith('R'):
-                return wordnet.ADV
-            else:
-                return wordnet.NOUN  # Assume nouns by default
-
-        # Initialize WordNet lemmatizer
-        lemmatizer = WordNetLemmatizer()
-
-        pos_tags = pos_tag(tokens)
-
-        # Lemmatize tokens with POS tags
-        lemmatized_tokens = [lemmatizer.lemmatize(token, get_wordnet_pos(pos_tag)) for token, pos_tag in pos_tags]
-        
-        # Join the lemmatized tokens back into a single string
-        lemmatized_text = ' '.join(lemmatized_tokens)
-        
-        return lemmatized_text
-
-
-    def filter_nontext(text):
-        text = remove_url(text)
-        text = remove_usermention(text)
-        text = remove_block_quotes(text)
-        text = remove_stacktrace(text)
-        # text = remove_newlines(text)
-        text = remove_triple_quotes(text)
-        # text = remove_extra_whitespaces(text)
-
-        return text.strip()
-
-    printable = set(string.printable)
-    text = ''.join(filter(lambda x: x in printable, text))
-    text = text.replace('\x00', ' ')  # remove nulls
-    text = text.replace('\r', ' ')
-    text = text.replace('\n', ' ')
-    text = re.sub('[^0-9a-zA-Z]+', ' ', text)
-    text = re.sub("(<.*?>)", " ", text)  # remove html markup
-    text = re.sub("(\\W|\\d)", " ", text)  # remove non-ascii and digits
-
-    text = text.lower()  # Lowercasing
-    text = text.strip()
-
-    # text = filter_nontext(text)
-    # text = remove_stopwords(text)
-    # text = lemmatize_text(text)
-    # text = text.strip()
-    
-    return text
-
-
-
-
-texts = []
-
-for i in range(len(dataframe['Sentence'].values.tolist())):
-    sentences = dataframe.iloc[i].values.tolist()
-    texts.append([text_cleaning(sentences[1]), text_cleaning(sentences[3]), text_cleaning(sentences[4])])
-
-dataframe = pd.DataFrame(texts, columns=['anchor', 'positive', 'negative'])
-
-print(dataframe)
-
-# Read the CSV file and extract the positive and negative samples
-ancors = []
-positives = []
-negatives = []
-
-for row in texts:
-    ancors.append (row[0])
-    positives.append (row[1])
-    negatives.append (row[2])
-
-    ancors.append (row[1])
-    positives.append (row[0])
-    negatives.append (row[2])
-
-
-print(len(positives))
-print(len(negatives))
-
-# =============================================================================================================
-
-import torch.optim as optim
-from torch.nn import functional as F
-from torch.utils.data import Dataset
+    dataset = TextDataset(ancors, positives, negatives, tokenizer, max_len=max_len)
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    return dataloader
 
 class TextDataset(Dataset):
     def __init__(self, anchors, positives, negatives, tokenizer, max_len):
@@ -306,18 +94,6 @@ class TextDataset(Dataset):
             'negative': negative['input_ids'].flatten(),
         }
 
-# Create a dataset and a dataloader
-dataset = TextDataset(ancors, positives, negatives, tokenizer, max_len=128)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-
-# Define a contrastive loss function
-
-# def contrastive_loss(anchor, positive, negative, margin=0.3):
-#     pos_sim = F.cosine_similarity(anchor, positive)
-#     neg_sim = F.cosine_similarity(anchor, negative)
-#     return torch.mean(F.relu(pos_sim - neg_sim + margin))
-
-
 def info_nce_loss(anchor_embeddings, positive_embeddings, negative_embeddings):
     # Compute the similarity between the anchor and the positive sample
     positive_sim = torch.nn.functional.cosine_similarity(anchor_embeddings, positive_embeddings)
@@ -330,41 +106,50 @@ def info_nce_loss(anchor_embeddings, positive_embeddings, negative_embeddings):
 
     return loss.mean()
 
+def train_model(model, dataloader, num_epochs=30, saved_file='model_and_optimizer.pt'):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
+    previous_loss = 100
 
+    model.train()
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for batch in dataloader:
+            anchor = model(batch['anchor'].to(device))[1]
+            positive = model(batch['positive'].to(device))[1]
+            negative = model(batch['negative'].to(device))[1]
 
-# Define an Adam optimizer and a learning rate scheduler
-optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
-previous_loss = 100
+            loss = info_nce_loss(anchor, positive, negative)
 
-# Train the model
-model.train()
-for epoch in range(30):
-    running_loss = 0.0
-    for batch in dataloader:
-        anchor = model(batch['anchor'].to(device))[1]
-        positive = model(batch['positive'].to(device))[1]
-        negative = model(batch['negative'].to(device))[1]
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        # loss = contrastive_loss(anchor, positive, negative)
+            running_loss += loss.item()
 
-        # Compute the InfoNCE loss
-        loss = info_nce_loss(anchor, positive, negative)
+        print(f'Epoch {epoch + 1} loss: {running_loss / len(dataloader)}')
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        if running_loss < previous_loss:
+            previous_loss = running_loss
+            # Save the model and optimizer
+            torch.save({'model_state_dict': model.state_dict()}, saved_file)
 
-        running_loss += loss.item()
+        if running_loss < 0.1:
+            break
 
-    print(f'Epoch {epoch + 1} loss: {running_loss / len(dataloader)}')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_name", help="Pretrained model name (bert, roberta, albert, codebert)")
+    parser.add_argument("data_file", help="Path to the data file")
+    parser.add_argument("output_save_path", help="Path to save the trained model and optimizer")
+    args = parser.parse_args()
 
-    if running_loss < previous_loss:
-        previous_loss = running_loss
-        # Save the model and optimizer
-        torch.save({
-            'model_state_dict': model.state_dict(),
-            # 'optimizer_state_dict': optimizer.state_dict(),
-        }, saved_file)
+    selected_model, saved_file = select_pretrained_model(args.model_name, args.output_save_path)
+    tokenizer = AutoTokenizer.from_pretrained(selected_model)
+    model = AutoModel.from_pretrained(selected_model)
 
-    if running_loss < 0.1:
-        break
+    dataframe = get_data(args.data_file)
+    dataloader = prepare_dataset(dataframe, tokenizer, max_len=128)
+
+    train_model(model, dataloader, num_epochs=30, saved_file=saved_file)
---
+++ contrastive_learning/readME.md
@@ -0,0 +1,61 @@
+# Contrastive Learning Training for Figurative Language
+
+This repository contains Python code to train a model using the InfoNCE loss function for learning text embeddings. The code uses the Hugging Face Transformers library for handling pretrained language models and PyTorch for training.
+
+## Overview
+
+The purpose of this code is to train a model using InfoNCE (InfoMax) loss, which is a contrastive learning objective that helps learn better text representations. It is particularly useful when dealing with limited labeled data.
+
+## Prerequisites
+
+Before running the code, make sure you have the following installed:
+
+1. Python (3.6 or higher)
+2. PyTorch (1.8.0 or higher)
+3. Transformers library from Hugging Face
+4. pandas library
+5. nltk library
+
+You can install the required libraries using pip:
+
+```bash
+pip install torch transformers pandas nltk
+```
+
+## Usage
+
+To run the training script, use the following command:
+
+```bash
+python run.py <model_name> <data_file> <output_save_path>
+```
+
+
+- `<model_name>`: Pretrained model name. Choose from "bert", "roberta", "albert", or "codebert".
+- `<data_file>`: Path to the CSV file containing the training data.
+- `<output_save_path>`: Path to save the trained model and optimizer.
+
+
+Example
+-------
+
+```
+python train_model.py bert dataset/Fig_Lan_Annotation.csv output
+```
+
+
+This will train a BERT-based model using the InfoNCE loss with the provided CSV data file and save the trained model and optimizer in the "output" directory.
+
+Notes
+-----
+
+1. The code supports different pretrained models from the Hugging Face Transformers library (BERT, RoBERTa, ALBERT, CodeBERT).
+2. The InfoNCE loss is used for training, which helps improve text embeddings in a self-supervised manner.
+3. The training process will stop if the loss falls below a threshold (0.1) or after 30 epochs, whichever comes first.
+
+Please ensure that you have enough GPU memory to run the training process if using a GPU. The code will automatically use the GPU if available; otherwise, it will use the CPU for training.
+
+Note: Make sure to customize the CSV file with your data before running the training script.
+
+Happy training!
+
